742b42b11d0c6a9d6e5ad82508dd0a17c6cdade7
jonathan-matheus <johhn.dev@gmail.com>
2023-06-15 16:49:26 -0300

    placar

--- sketch.js
+++ sketch.js
@@ -145,11 +145,17 @@ function movimentaRaqueteOponente(){
  * @returns {void} Esta função não retorna nada
  */
 function incluiPlacar(){
+  stroke(255);
   textAlign(CENTER);
   textSize(16);
+  fill(color(255, 140, 0));
+  rect(150, 10, 40, 20);
   fill(255);
-  text(meusPontos, 278, 26);
-  text(pontosDoOponente, 321, 26);
+  text(meusPontos, 170, 26);
+  fill(color(255, 140, 0));
+  rect(450, 10, 40, 20);
+  fill(255);
+  text(pontosDoOponente, 470, 26);
 }
 
 /**
